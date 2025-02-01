b08653ff3cc312b179a3156bf39d91e81b205192
prasanna192005 <prasannapandharikar19@gmail.com>
2025-02-01 17:00:32 +0530

    meow

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -72,19 +72,27 @@ export const FloatingNav = ({
         }}
       >
         {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            {/* add !cursor-pointer */}
-            {/* remove hidden sm:block for the mobile responsive */}
-            <span className=" text-sm !cursor-pointer">{navItem.name}</span>
-          </Link>
-        ))}
+  <Link
+    key={`link=${idx}`}
+    href={navItem.link}
+    className={cn(
+      "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+    )}
+  >
+    <span className="block sm:hidden">{navItem.icon}</span>
+    
+    {/* Hide "Visualise" on mobile */}
+    <span
+      className={cn(
+        "text-sm !cursor-pointer",
+        navItem.name === "Visualize" ? "hidden sm:block":""
+      )}
+    >
+      {navItem.name}
+    </span>
+  </Link>
+))}
+
         {/* remove this login btn */}
         {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Login</span>
